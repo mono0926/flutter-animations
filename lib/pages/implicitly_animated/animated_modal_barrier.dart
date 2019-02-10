027a54de2b4507d2c93313712111667031320914
Masayuki Ono <mono0926@gmail.com>
2019-02-10 18:51:41 +0900

    Implement AnimatedModalBarrier example

--- lib/pages/implicitly_animated/animated_modal_barrier.dart
+++ lib/pages/implicitly_animated/animated_modal_barrier.dart
@@ -5,26 +5,115 @@ class AnimatedModalBarrierPage extends StatefulWidget {
   static const routeName = 'AnimatedModalBarrier';
 
   @override
-  _AnimatedModalBarrierPageState createState() =>
-      _AnimatedModalBarrierPageState();
+  _AnimatedModalBarrierPageState createState() {
+    return new _AnimatedModalBarrierPageState();
+  }
 }
 
 class _AnimatedModalBarrierPageState extends State<AnimatedModalBarrierPage> {
-  var _index = 0;
-
+  var _dismissible = false;
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
       title: 'AnimatedModalBarrier',
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _index++;
-          });
-        },
-        child: const Icon(Icons.refresh),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              child: const Text('Open Modal View'),
+              onPressed: () {
+                Navigator.of(context).push(MyPageRoute(
+                  page: _ModalPage(),
+                  dismissible: _dismissible,
+                ));
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('dismissible'),
+                Switch(
+                  onChanged: (value) {
+                    setState(() {
+                      _dismissible = value;
+                    });
+                  },
+                  value: _dismissible,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      child: AnimatedModalBarrier(),
+    );
+  }
+}
+
+class MyPageRoute extends TransitionRoute {
+  MyPageRoute({
+    @required this.page,
+    @required this.dismissible,
+  });
+
+  final Widget page;
+  final bool dismissible;
+
+  @override
+  Iterable<OverlayEntry> createOverlayEntries() {
+    return [
+      OverlayEntry(builder: _buildModalBarrier),
+      OverlayEntry(builder: (context) => Center(child: page))
+    ];
+  }
+
+  @override
+  bool get opaque => false;
+
+  @override
+  Duration get transitionDuration => Duration(milliseconds: 500);
+
+  Widget _buildModalBarrier(BuildContext context) {
+    return IgnorePointer(
+      ignoring: animation.status ==
+              AnimationStatus
+                  .reverse || // changedInternalState is called when this updates
+          animation.status ==
+              AnimationStatus
+                  .dismissed, // dismissed is possible when doing a manual pop gesture
+      child: AnimatedModalBarrier(
+        color: animation.drive(
+          ColorTween(
+            begin: Colors.transparent,
+            end: Colors.black.withAlpha(80),
+          ),
+        ),
+        dismissible: dismissible,
+      ),
+    );
+  }
+}
+
+class _ModalPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('(　´･‿･｀)'),
+            const SizedBox(height: 8),
+            RaisedButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
