96e6cead5772153f92361d0ea0543cee42c06c61
mono <mono0926@gmail.com>
2022-08-25 16:49:09 +0900

    Update packages

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mono/fvm/versions/stable"
+export "FLUTTER_ROOT=/Users/mono/fvm/versions/beta"
 export "FLUTTER_APPLICATION_PATH=/Users/mono/Git/flutter-animations"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
@@ -8,6 +8,6 @@ export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.1.0"
 export "FLUTTER_BUILD_NUMBER=1.1.0"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.dart_tool/package_config.json"
